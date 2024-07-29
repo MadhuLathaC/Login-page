250c901aa4fbb59d5f83a7ae6424943fca5ecf3e
MadhuLathaC <chethipattumadhulatha@gmail.com>
2024-07-29 12:53:13 +0530

    first commit

---
+++ loginPage/src/components/LoginPage.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './LoginPage.css'
+import { FaUser, FaLock } from "react-icons/fa";
+
+
+const LoginPage = () => {
+  return (
+    <div className='wrapper'>
+        <form action="">
+            <h1>Login</h1>
+            <div className='input-box'>
+            <input type="text" placeholder='UserName' required/>
+            <FaUser className='icon'/>
+            </div>
+
+            <div className='input-box'>
+            <input type="password" placeholder='PassWord'required/>
+            <FaLock className='icon' />
+            </div>
+
+            <div className='remember-forgot'>
+                <label> <input type="checkbox"/>Remember me</label>
+                <a href="#">forgot password</a>
+            </div>
+
+            <button type="submit">login</button>
+
+            <div className='register-link'>
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </div>
+
+
+            
+
+            
+
+        </form>
+
+    </div>
+  )
+}
+
+export default LoginPage
